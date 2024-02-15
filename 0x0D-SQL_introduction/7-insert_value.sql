5f491a18a865fed4837001cf97fc4f08a3898df2
Salmarh1 <salmarhadi5@gmail.com>
2024-02-15 15:38:36 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---inserts a new into the table first_table.
-Insert into 'first_table' ('id', 'name') values (89, 'Best School');
+-- inserts a new into the table first_table.
+ Insert into 'first_table' ('id', 'name') values (89, 'Best School');
