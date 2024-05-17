d9bcbb13433d18963ba3dd2aa7d9e9f4fd3a558e
Salmarh1 <salmarhadi5@gmail.com>
2024-05-17 14:13:46 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,22 +4,28 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
+async function getCharacters() {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      request.get(url, (error, response, body) => {
+        if (error) reject(error);
+        else resolve(JSON.parse(body));
       });
+    });
+
+    const characters = response.characters;
+    for (const character of characters) {
+      const characterResponse = await new Promise((resolve, reject) => {
+        request.get(character, (error, response, body) => {
+          if (error) reject(error);
+          else resolve(JSON.parse(body).name);
+        });
+      });
+      console.log(characterResponse);
     }
+  } catch (error) {
+    console.log(error);
   }
-});
+}
+
+getCharacters();
